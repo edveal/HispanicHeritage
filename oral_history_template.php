b20cfd468f05aeb0d5276c2183d0216eefab4d93
Edward Veal <edward.m.veal@gmail.com>
2017-08-18 13:49:04 -0500

    Oral History template
    This is the first upload of a .php file I wrote and am converting to a template for future use.

---
+++ oral_history_template.php
@@ -0,0 +1,228 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=7"> 
+<meta http-equiv="X-UA-Compatible" content="IE=8"> 
+<meta http-equiv="X-UA-Compatible" content="IE=9">
+
+  <title>HMRC Film Archive</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="http://digital.houstonlibrary.net/film-archive/digitized-film-new.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  
+<style>
+.container_banner {
+    background-color: #BF282D;
+	padding-right: 15px;
+    padding-left: 35px;
+    margin-right: auto;
+    margin-left: auto;
+}
+	.banner {
+    background-color: #BF282D;
+}
+
+
+
+.disclaimer {
+    position: relative;
+    font-family: Arial, Helvetica, sans-serif;
+    height: 240px;
+    margin-top: 2px;
+}
+
+
+
+.transcript1 {
+    float: right;
+    background-color: #dbdaca;
+    padding-top: 5px;
+    padding-right: 10px;
+    padding-bottom: 10px;
+    padding-left: 20px;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 70%;
+    overflow: auto;
+    border: 1px solid #CCCCCC;
+    margin-top: 8px;
+}
+
+.footer {
+	background-color: #BF282D;
+	width: 100%;
+	padding-top: 8px;
+	padding-bottom: 9px;
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 100%;
+	font-weight: normal;
+	color: #FFFFFF;
+	border-top-width: 1px;
+	border-bottom-width: 1px;
+	border-top-style: solid;
+	border-bottom-style: solid;
+	border-top-color: #000000;
+	border-bottom-color: #000000;
+	float: left;
+}
+.footer a:link {
+	color: #FFFFFF;
+	text-decoration: none;
+}
+.footer a:visited {
+	color: #FFFFFF;
+	text-decoration: none;
+}
+.footer a:hover {
+	color: #191919;
+	text-decoration: underline;
+}
+.footer a:active {
+	color: #191919;
+	text-decoration: none;
+}
+
+</style>
+  
+<!-- Scripts -->
+ <script language="javaScript" type="text/javascript" src="show.js"></script>
+
+<script type="text/javascript" src="jwplayer.js"></script>
+<script type="text/javascript">jwplayer.key="1HnKWXUiT3lFtXxtv8KZSHJEjISVxDnax9nOduhkZpI=";</script>
+
+<script type="text/javascript">
+<!--
+function showlayer(layer){
+var myLayer = document.getElementById(layer).style.display;
+ if(myLayer=="none"){
+ document.getElementById(layer).style.display="block";
+ } else { 
+document.getElementById(layer).style.display="block";
+ }
+}
+-->
+</script>
+
+<script type="text/javascript">
+<!--
+function showlayer2(layer){
+var myLayer = document.getElementById(layer).style.display;
+ if(myLayer=="block"){
+ document.getElementById(layer).style.display="none";
+ } else { 
+document.getElementById(layer).style.display="none";
+ }
+}
+-->
+</script>
+  
+  </head>
+<body>
+
+
+
+<div class="container-fluid">
+	<div  class="container_banner">
+		<div class="banner"><a href="http://digital.houstonlibrary.org/cdm/"><img class="img-responsive" src="http://digital.houstonlibrary.net/hispanic-collections/images/banner4.gif" alt="" width="738" height="78" border="0" /></a></div>
+	</div>
+	
+
+<div class="col-sm-12">
+
+<div class="col-sm-2"></div>
+<div class="col-sm-4">
+<div class="lft_float_rgt">
+
+  <div id="notice">Please read and accept the disclaimer below to continue.
+ 
+ <br>
+<br>
+<noscript>
+<div class="scriptoff">JavaScript is currently turned off in your web browser. <br>
+Please turn it on to take full advantage of this site, then refresh the page.
+</div>
+</noscript>
+ 
+ 
+ </div>
+ 
+<div id="clip">
+
+<!--<div style="display: none;" id="myName">-->
+<div style="display: none;" id="myName">
+
+<div class="player">
+  <div id="myElement">Loading the player...</div>
+
+<script type="text/javascript">
+    jwplayer("myElement").setup({
+        file: "media/OH0595.mp4",
+        image: "images/OH0595.JPG",
+		controls: true,
+		width: 320,
+      height: 240
+
+    });
+</script>
+</div>
+</div>
+
+
+<!--end clip-->
+</div>
+
+
+<div class="disclaimer">
+
+ <!-- <div class="alert"><strong>The Oral History Streaming Video presentations are currently offline, we are working to address this problem and hope to have it resolved as soon as possible. We do apologize for this inconvenience.</strong></div>
+<br />
+-->
+<p><strong>DISCLAIMER</strong><br />
+</p>
+<p><a href="javascript:void(0);" onclick="javascript:showlayer('myName')" onmouseup="javascript:showlayer2('notice')">I have read and accept the disclaimer terms</a><br />
+</p>
+<p>The Houston Public  Library offers public access to a wide range of information, including  historical materials that are products of their particular times and may  contain offensive language or negative stereotypes. All historical media are  presented as specific, original artifacts without further enhancement to their  appearance or quality, as a record of the era in which they were produced. Any  viewpoints or opinions expressed in these materials are not necessarily those  of the City of Houston, the Houston Public Library or any of its officers,  agents, employees, or volunteers. The City of Houston and the Houston Public  Library make no warranty as to the accuracy or completeness of any information  contained in the materials  and expressly disclaim any liability therefore.<br />
+</p>
+<p>The Houston Public  Library provides unedited versions of all films from its archives. Some parents  may find material objectionable for minors. Parents are encouraged to interact  with their children as they use the <a href="http://digital.houstonlibrary.org/" target="_blank">Houston  Area Digital Archives</a> web site to complete research and homework  activities.<br />
+<br />
+The Houston Public Library retains the literary and publishing rights of its films.  No reels, clips, excerpts or transcripts may be published without the written  permission of the Houston Metropolitan Research Center.<br />
+<br />
+Requests for copies or for permission to quote for publication should be  directed to the Houston Metropolitan Research Center at TXR.Reference@houstontx.gov or  (832) 393-1662.</p>
+<p><a href="javascript:void(0);" onclick="javascript:showlayer('myName')" onmouseup="javascript:showlayer2('notice')">I have read and accept the terms of the disclaimer</a></p>
+<p><br>
+</p>
+</div>
+<br clear="all" />
+</div>
+</div>
+
+<div class="col-sm-4">
+
+	<div class="transcript1">
+				  <p><strong>Interview with: Rodarte, Luis</strong><br />
+				  <strong>Interviewed by: Selley, Mikaela; Adalba, Edith; Valdez, Martin</strong>: <br />
+					Date: 3/10/2017 <br />
+					Archive Number: </strong></p>
+					<!--<p><a href="javascript:void(0);" onclick="jwplayer(myElement).seek(192)"><img border="0" alt="cue point" align="absmiddle" src="media/watch2.gif" width="21" height="25" /></a><br />
+					<br /> -->
+				  </p>
+	</div>
+
+</div>
+
+<div class="col-sm-2"></div>
+</div>
+
+<div class="col-sm-12">
+<div class="footer">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://digital.houstonlibrary.org/cdm/">Digital Archives Home</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href="http://www.houstonlibrary.org/home">Houston Public Library</a></div>
+<p><br />
+</p>
+</div>
+	
+</body>
+</html>
